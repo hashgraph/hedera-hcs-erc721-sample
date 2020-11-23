9125ffe78cf493dda43fe65bdc2afc1117f8573b
Ryan Leckey <ryan@launchbadge.com>
2020-11-23 10:01:00 -0800

    Implement primitive Int and Address types

--- contract/src/main/java/com/hedera/hashgraph/seven_twenty_one/contract/Address.java
+++ contract/src/main/java/com/hedera/hashgraph/seven_twenty_one/contract/Address.java
@@ -1,3 +1,44 @@
 package com.hedera.hashgraph.seven_twenty_one.contract;
 
-public class Address {}
+import com.google.protobuf.ByteString;
+import com.hedera.hashgraph.sdk.PublicKey;
+
+public final class Address {
+
+    public final PublicKey publicKey;
+
+    public Address(PublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public static Address fromString(String text) {
+        return new Address(PublicKey.fromString(text));
+    }
+
+    public static Address fromByteString(ByteString data) {
+        return new Address(PublicKey.fromBytes(data.toByteArray()));
+    }
+
+    public ByteString toByteString() {
+        return ByteString.copyFrom(publicKey.toBytes());
+    }
+
+    @Override
+    public String toString() {
+        return publicKey.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Address otherAddress = (Address) o;
+        return publicKey.equals(otherAddress.publicKey);
+    }
+
+    @Override
+    public int hashCode() {
+        return publicKey.hashCode();
+    }
+}
--- contract/src/main/java/com/hedera/hashgraph/seven_twenty_one/contract/Int.java
+++ contract/src/main/java/com/hedera/hashgraph/seven_twenty_one/contract/Int.java
@@ -1,3 +1,58 @@
 package com.hedera.hashgraph.seven_twenty_one.contract;
 
-public class Int {}
+import com.google.protobuf.ByteString;
+import java.math.BigInteger;
+
+public final class Int {
+
+    public static final Int MAX = new Int(new BigInteger("2").pow(256));
+
+    public static final Int ZERO = new Int(BigInteger.ZERO);
+
+    public final BigInteger value;
+
+    public Int(BigInteger value) {
+        if (value.compareTo(MAX.value) > 0) {
+            // this is *greater than* Int.MAX
+            throw new IllegalArgumentException(
+                "integers must not exceed 2^256 or " + MAX
+            );
+        }
+
+        if (value.compareTo(ZERO.value) < 0) {
+            // this is *less than* zero
+            throw new IllegalArgumentException(
+                "integers must not be less than zero"
+            );
+        }
+
+        this.value = value;
+    }
+
+    public static Int fromByteString(ByteString data) {
+        return new Int(new BigInteger(data.toByteArray()));
+    }
+
+    public ByteString toByteString() {
+        return ByteString.copyFrom(value.toByteArray());
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        var otherInt = (Int) o;
+        return value.equals(otherInt.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
